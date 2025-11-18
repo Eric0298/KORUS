7d036128d70a723e717b13b2b215a4a1dba1e724
Eric Mancebo <ericmancebo6950@gmail.com>
2025-11-18 18:50:49 +0100

    establecida la conexion del proyecto a Mongo Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
-
+connectDB();
 // Middlewares básicos
 app.use(cors());
 app.use(express.json());
